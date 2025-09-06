a97a198af56c7e0a872dcb991bf2d00223d2524c
Tom <tomregev808@gmail.com>
2025-09-06 22:24:53 +0300

    created a brach with a simple  that only displays the name of a random place on the page, nothing else, simple and get the work done

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import requests
 
 
 import random
+from flask import Flask
+import os
 
 def get_name ():
 
@@ -18,4 +20,33 @@ def get_name ():
         names.append (record ["שם_ישוב"])
 
     name = random.choice(names)
+
+    return name
+
+
+
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
+    )
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent = True)
+    else:
+        app.config.from_mapping (test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/', methods=['get', 'post'])
+    def index ():
+        return (get_name())
+
+    return app
+
 
