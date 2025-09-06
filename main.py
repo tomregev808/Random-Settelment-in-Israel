2ba8a7344da6f4aa82859eccef02c3f12c2ddc2d
Tom <tomregev808@gmail.com>
2025-09-06 22:15:15 +0300

    created a function that gets a random settelment in israel from the government api

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+
+import random
+
+def get_name ():
+
+
+    d = requests.get ("https://data.gov.il/api/3/action/datastore_search?resource_id=5c78e9fa-c2e2-4771-93ff-7f400a12f7ba")
+
+    dict = d.json()
+
+    list = dict["result"]["records"]
+    list.pop(0)
+
+    names = []
+    for record in list:
+        names.append (record ["שם_ישוב"])
+
+    name = random.choice(names)
+
